cbf3da67609718ffe163f35849335b32cec7b407
79255@DESKTOP-A17MI81 <Don20034562@bk.ru>
2023-05-25 21:12:59 +0300

    1_4

---
+++ HW_1/1_4.py
@@ -0,0 +1,10 @@
+n = int(input("Введите значение n: "))
+m = int(input("Введите значение m: "))
+k = int(input("Введите значение k: "))
+if n * m >= k:
+    if k % n == 0 or k % m == 0:
+        print('Yes')
+    else:
+        print('No')
+else:
+    print('No')
