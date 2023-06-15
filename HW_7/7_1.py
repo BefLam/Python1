1464f74f8fb8c853cb4a977c7c0a688598265841
79255@DESKTOP-A17MI81 <Don20034562@bk.ru>
2023-06-16 00:14:40 +0300

    7_1

---
+++ HW_7/7_1.py
@@ -0,0 +1,13 @@
+s = input().split()
+counts = []
+for phrase in s:
+    vowels = 0
+for letter in phrase:
+    if letter in 'aeiouyAEIOUY':
+        vowels += 1
+counts.append(vowels)
+
+if len(set(counts)) == 1:
+    print("Парам пам-пам")
+else:
+    print("Пам парам")
